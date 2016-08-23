e44eb39a9d77b575bfb61472ae28c336bcefafd7
Jeff Lindsay <progrium@gmail.com>
2016-08-23 09:44:01 -0500

    Update slackscript.js

--- slackscript.js
+++ slackscript.js
@@ -2,19 +2,19 @@ slack.on("slashCommand", function(cmd) {
 	cmd.on("hello")
 		.description("says hello")
 		.action(function(params) {
-			cmd.reply("hello yourself");
+			cmd.publicReply("hello yourself");
 		});
 
 	cmd.on("goodbye")
 		.description("says goodbye")
 		.action(function(params) {
-			cmd.reply("goodbye, "+cmd.userName);
+			cmd.publicReply("goodbye, "+cmd.userName);
 		});
 
 	cmd.on("show source")
 		.description("shows script source")
 		.action(function(params) {
-			cmd.reply(slack.preformatted(__script));
+			cmd.publicReply(slack.preformatted(__script));
 		});
 
 	cmd.run();
